23e545bddf7dc0f14c8b3534dfa8a7de55ba6ca5
akiaki touka <35886966+akiakishitai@users.noreply.github.com>
2019-08-10 19:28:07 +0900

    Fix /sys/poweroff

--- internal/api_poweroff.go
+++ internal/api_poweroff.go
@@ -1,6 +1,7 @@
 package openapi
 
 import (
+	"encoding/json"
 	"strings"
 	"io/ioutil"
 	"log"
@@ -11,7 +12,6 @@ import (
 // SysPoweroff - System shutdown or reboot
 func SysPoweroff(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 
 	cmds := map[Power]*(exec.Cmd){
 		SHUTDOWN: exec.Command("sleep", "3s", "&&", "poweroff"),
@@ -20,13 +20,30 @@ func SysPoweroff(w http.ResponseWriter, r *http.Request) {
 
 	bs, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(unexpectedError)
 	}
 	
 	v, ok := cmds[Power(strings.ToLower(string(bs)))]
-	if ok {
-		v.Start()
-	} else {
-		log.Print("Not allowed this request. Please 'shutdown' or 'reboot'")
+	if !ok {
+		msg := "Not allowed this request. Please 'shutdown' or 'reboot'"
+		res := Error{
+			Code: 400,
+			Message: msg,
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(res)
+		log.Print(msg)
+		return
 	}
+
+	if err = v.Start(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(unexpectedError)
+		log.Print(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
